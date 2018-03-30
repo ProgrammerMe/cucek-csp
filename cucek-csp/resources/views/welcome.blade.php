3ad4847002247c32cf09cd572a5e9b2e9c9ac7d1
Siddharth M <siddharth.m98@gmail.com>
2018-03-30 20:00:32 +0530

    0.0.045 basic front page ready

--- cucek-csp/resources/views/welcome.blade.php
+++ cucek-csp/resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <div class="TopLogo">
@@ -219,13 +220,16 @@
 <br>
 <hr>
 <footer>
-  <nav>
-    <ul class="end">
-      <li>team</li>
-      <li>copyright</li>
-    </ul>
-  </nav>
-
+  <br>
+  <div class="row">
+    <div class="col-9 coffee">
+ <a href="#"><i class="fa fa-coffee fa-3x" aria-hidden="true"></i></a>
+ </div>
+ <div class="col-3 copyright">
+ <p><i class="fa fa-copyright" aria-hidden="true"></i> 2018 , CSE-CUCEK</p>
+ </div>
+ </div>
+<br>
 </footer>
 
 
